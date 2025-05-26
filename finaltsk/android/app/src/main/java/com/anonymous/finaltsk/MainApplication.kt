3847fef61c81005a9284dbda780384e449a12dfb
Glenn <glenn.liuchanghui@gmail.com>
2025-05-26 16:57:04 +0800

    backend (Firebase)

--- finaltsk/android/app/src/main/java/com/anonymous/finaltsk/MainApplication.kt
+++ finaltsk/android/app/src/main/java/com/anonymous/finaltsk/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.finaltsk
+package com.anonymous.finaltsk
 
 import android.app.Application
 import android.content.res.Configuration
--- finaltsk/app/_layout.tsx
+++ finaltsk/app/_layout.tsx
@@ -1,5 +1,10 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return(
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }}/>
+      <Stack.Screen name="sign-up" options={{ headerShown: false }}/>
+    </Stack>
+  )
 }
